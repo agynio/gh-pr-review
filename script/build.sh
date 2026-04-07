2d8cf8ced7f4fe8cdf1757081c8b01bfae68b6c5
Nicolas Marchildon <nicolas.marchildon@mobilityhouse.com>
2026-04-07 18:50:16 -0400

    fix: use standard platform names in dist/

--- script/build.sh
+++ script/build.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 set -e
 
-VERSION=${1#v}
-
 mkdir -p dist
 
 # Build for Windows
-GOOS=windows GOARCH=amd64 go build -ldflags="-s -w" -o dist/gh-pr-review_${VERSION}_windows-amd64.exe .
+GOOS=windows GOARCH=amd64 go build -trimpath -ldflags="-s -w" -o dist/windows-amd64.exe .
 
 # Build for macOS
-GOOS=darwin GOARCH=amd64 go build -ldflags="-s -w" -o dist/gh-pr-review_${VERSION}_darwin-amd64 .
-GOOS=darwin GOARCH=arm64 go build -ldflags="-s -w" -o dist/gh-pr-review_${VERSION}_darwin-arm64 .
+GOOS=darwin GOARCH=amd64 go build -trimpath -ldflags="-s -w" -o dist/darwin-amd64 .
+GOOS=darwin GOARCH=arm64 go build -trimpath -ldflags="-s -w" -o dist/darwin-arm64 .
 
 # Build for Linux
-GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o dist/gh-pr-review_${VERSION}_linux-amd64 .
-GOOS=linux GOARCH=arm64 go build -ldflags="-s -w" -o dist/gh-pr-review_${VERSION}_linux-arm64 .
+GOOS=linux GOARCH=amd64 go build -trimpath -ldflags="-s -w" -o dist/linux-amd64 .
+GOOS=linux GOARCH=arm64 go build -trimpath -ldflags="-s -w" -o dist/linux-arm64 .
